fae0397af44d9f858a882c714842afd11062ea8a
ronald0204 <raguirrevil0204@gmail.com>
2020-06-10 19:22:37 +0000

    Three

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-return (0);   
+    return (0);
 }
