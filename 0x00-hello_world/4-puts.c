8437d50d04bc9e6b81bb85af7a96bd11dbb9d6e9
ronald0204 <raguirrevil0204@gmail.com>
2020-06-10 19:06:00 +0000

    My Firts Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-
-
-
+#include<stdio.h>
+/**
+ * My Firts Programming
+ */
 int main(void)
 {
-    
-    puts("\"Programming is like building a multilingual puzzle");
-   
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
