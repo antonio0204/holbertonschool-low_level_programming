8126f1d26c65cce5d982f2a66ebef5e012d531a2
ronald0204 <raguirrevil0204@gmail.com>
2020-06-12 03:12:57 +0000

    Task #6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - Runs the file and if the n > 0 prints positive and
+  * n < 0 negative,else it will print 0
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int numb;
+
+	for (numb = 48; numb <= 57; numb++)
+	{
+		putchar(numb);
+	}
+	putchar('\n');
+	return (0);
+}
