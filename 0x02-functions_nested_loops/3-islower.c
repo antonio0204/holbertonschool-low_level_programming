185061dfd3c874f113c0a7128dec73cd3bdec1e6
ronald0204 <raguirrevil0204@gmail.com>
2020-06-16 16:49:09 +0000

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
-* main - check the code for Holberton School students.
+* _islower - check the code for Holberton School students.
 *
+* @c:integer
 * Return: Always 0.
 */
 int _islower(int c)
@@ -16,4 +17,3 @@ int _islower(int c)
 
 
 }
-
