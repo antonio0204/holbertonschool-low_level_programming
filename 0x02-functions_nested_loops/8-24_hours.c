2996642a0daf9dea8a0b572487db9c902e2fe551
ronald0204 <raguirrevil0204@gmail.com>
2020-06-16 22:05:25 +0000

    Task #8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* jack_bauer - check the code for Holberton School students.
+*
+* Return: void.
+*/
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
