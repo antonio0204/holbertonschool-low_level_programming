588886f8cdf7be417d20dfd49b7ec7ece11745f6
ronald0204 <raguirrevil0204@gmail.com>
2020-06-17 01:18:25 +0000

    Task #10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* add - check the code for Holberton School students.
+* @a : gets the firts number
+* @b : gets the second number
+* Return: Always 0.
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
