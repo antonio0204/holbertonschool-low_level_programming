ebc45344dc06f4e073722cdbb5ad6d0d61a60917
ronald0204 <raguirrevil0204@gmail.com>
2020-06-17 00:12:11 +0000

    Library

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -52,7 +52,12 @@ int print_last_digit(int);
 * Return: Always 0.
 */
 void jack_bauer(void);
-
+/**
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
+void times_table(void);
 
 
 
