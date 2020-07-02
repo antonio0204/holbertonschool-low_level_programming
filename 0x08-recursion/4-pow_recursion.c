65bb200e75cceda5dec551b999974c23048388fe
ronald0204 <raguirrevil0204@gmail.com>
2020-07-02 02:42:21 +0000

    Task #4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _pow_recursion - gives x^y
+*@x: int
+*@y: int
+*Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * (_pow_recursion(x, y - 1)));
+}
