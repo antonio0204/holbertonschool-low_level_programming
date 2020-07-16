3eeb202dc8aba321f652c2b85a967214a185843f
ronald0204 <raguirrevil0204@gmail.com>
2020-07-16 02:18:56 +0000

    Requeriment and Task #0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints a name
+ *@name: name string
+ *@f: pointer to function (char *)
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		f(name);
+}
