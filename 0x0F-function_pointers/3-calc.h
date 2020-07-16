b5d0a577e029848dd55f6ccc5ebf3fcf762d530e
ronald0204 <raguirrevil0204@gmail.com>
2020-07-16 03:25:49 +0000

    Task #3 Calc

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,7 @@
-#ifndef CALC_H
-#define CALC_H
-
+#ifndef _CALC_H
+#define _CALC_H
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
@@ -20,5 +18,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
-
-#endif /* CALC_H */
+#endif /*_CALC_H*/
