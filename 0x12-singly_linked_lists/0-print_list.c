62b456d6eb1448eed33238bfb7650e77e95e87dd
ronald0204 <raguirrevil0204@gmail.com>
2020-07-24 03:04:21 +0000

    Task #0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - Print all the elements of a list
+ * @h: head pointer to list
+ * Return: N
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *buff;
+	unsigned int i;
+
+	buff = h;
+	for (i = 0; buff; i++)
+	{
+		if (buff->str)
+			printf("[%u] %s\n", buff->len, buff->str);
+		else
+			printf("[%u] %s\n", 0, "(nil)");
+		buff = buff->next;
+	}
+	return (i);
+}
