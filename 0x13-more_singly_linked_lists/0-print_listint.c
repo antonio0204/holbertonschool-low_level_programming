75cac6308ffadded0688857b10b33ec223ef3e9a
ronald0204 <raguirrevil0204@gmail.com>
2020-07-28 22:34:09 +0000

    Correction Task #0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_listint -  prints all.
+ * print_listint -  prints listint.
  * @h: list to print
  * Return: number
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t a = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 	printf("%d\n", h->n);
-		a++;
+		count++;
 		h = h->next;
 	}
-	return (a);
+	return (count);
 }
