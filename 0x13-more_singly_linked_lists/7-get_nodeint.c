4a46c6a51510286a375564237dd6ab17b9d1fd8b
ronald0204 <raguirrevil0204@gmail.com>
2020-07-29 04:28:48 +0000

    Task #7.3

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 /**
-*get_nodeint_at_index - gets the number of the index given.
+*get_nodeint_at_index - gets the number of the index.
 *@head: the list
 *@index: the number of the node I will get
 *Return: the node in the index given.
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr;
+	listint_t *pt;
 	unsigned int cont = 0;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	ptr = head;
-	while (ptr != NULL)
+	pt = head;
+	while (pt != NULL)
 	{
 		if (cont == index)
 		{
 			return (ptr);
 		}
-		ptr = ptr->next;
+		pt = pt->next;
 		cont++;
 	}
 	return (NULL);
