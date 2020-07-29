c0030b4b26dcaa0bb1cc85bca145f9b4e6e11d5e
ronald0204 <raguirrevil0204@gmail.com>
2020-07-29 01:47:01 +0000

    Task #1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len -  the number of elements.
+ * @h: header
+ * Return: n
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t a = 0;
+
+	while (h != NULL)
+	{
+		a++;
+		h = h->next;
+	}
+	return (a);
+}
