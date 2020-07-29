9b38cbe5d528912af3a327029477e1db44b7fd0e
ronald0204 <raguirrevil0204@gmail.com>
2020-07-29 03:07:38 +0000

    Task #5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint2 - ver2
+ *@head: head
+ *Return: address.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
