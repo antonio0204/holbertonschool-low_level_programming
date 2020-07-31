6c1c2cab81b03765fa11860fc41c2b0c61f235b2
ronald0204 <raguirrevil0204@gmail.com>
2020-07-31 03:49:28 +0000

    Task #1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * _pow - calculates a ^ b
+ * @a: base
+ * @b: power
+ * Return: int
+ */
+unsigned long int _pow(unsigned int a, unsigned int b)
+{
+	unsigned long int re;
+	unsigned int i;
+
+	re = 1;
+
+	for (i = 1; c <= b; i++)
+		rez *= a;
+	return (re);
+}
+/**
+ * print_binary - prints a number in binary notation
+ * @n: int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a;
+	char f = 0;
+
+	d = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (a != 0)
+	{
+		if ((n & a) == a)
+		{
+			f = 1;
+			_putchar('1');
+		}
+		else if (f == 1 || a == 1)
+			_putchar('0');
+		a >>= 1;
+	}
+}
