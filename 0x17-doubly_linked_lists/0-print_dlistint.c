a5e5e667c31a3e1e20e3ad6f3e901db71494795a
ronald0204 <raguirrevil0204@gmail.com>
2020-09-06 23:41:06 +0000

    Task #0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *print_dlistint-prints all the elements of a dlistint_t list
+ *
+ *@h: linked list
+ *
+ *Return: number of node
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t i = 0;
+
+
+if (!h)
+return (0);
+
+while (h != NULL)
+{
+
+i++;
+printf("%d\n", h->n);
+h = h->next;
+}
+
+return (i);
+}
